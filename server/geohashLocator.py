409ebe75888efbb83a468f3f64ed34bad30b99f4
user <user@debian.domain>
2018-04-05 20:01:17 +0000

    fixed typo

--- server/geohashLocator.py
+++ server/geohashLocator.py
@@ -11,7 +11,7 @@ class GeohashLocator(ALocator):
         However, results usually also include locations which are slightly more distant than desired.
         Consider using HybridLocator which runs a search with GeohashLocator and further filters the initial larger set of results with a more precise distance based BasicLocator.
         In addition, geohash values of close locations differ significantly around polar and equatorial regions as well as prime meridian and +-180 meridians.
-        Caonsider using HybridLocator which falls back to using BasicLocator when a search is being conducted around the specified regions.
+        Consider using HybridLocator which falls back to using BasicLocator when a search is being conducted around the specified regions.
     """
     def __init__(self, itemCollection):
         self.container = GeohashContainer(itemCollection)
